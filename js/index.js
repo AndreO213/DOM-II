ccbc9059e20d6e745821d504c5fd89674ee1a3e1
Andre Orellana <andre@Andres-MacBook-Pro.local>
2020-11-04 11:51:11 -0700

    Almost finished with the MVP

--- js/index.js
+++ js/index.js
@@ -1,1 +1,89 @@
 // Your code goes here
+let tabs = document.querySelectorAll('a');
+
+tabs[0].addEventListener('mouseover', event => {
+    event.target.style.color = 'blue';
+    event.target.style.fontSize = '5rem'
+});
+tabs[0].addEventListener('mouseleave', event => {
+    event.target.style.color = 'black';
+    event.target.style.fontSize = '1.5rem';
+});
+
+tabs[1].addEventListener('mouseover', event => {
+    event.target.style.color = 'blue';
+    event.target.style.fontSize = '5rem'
+});
+tabs[1].addEventListener('mouseleave', event => {
+    event.target.style.color = 'black';
+    event.target.style.fontSize = '1.5rem';
+});
+
+tabs[2].addEventListener('mouseover', event => {
+    event.target.style.color = 'blue';
+    event.target.style.fontSize = '5rem'
+});
+tabs[2].addEventListener('mouseleave', event => {
+    event.target.style.color = 'black';
+    event.target.style.fontSize = '1.5rem';
+});
+
+tabs[3].addEventListener('mouseover', event => {
+    event.target.style.color = 'blue';
+    event.target.style.fontSize = '5rem'
+});
+tabs[3].addEventListener('mouseleave', event => {
+    event.target.style.color = 'black';
+    event.target.style.fontSize = '1.5rem';
+});
+
+
+
+
+
+let logo = document.querySelector("h1")
+
+logo.addEventListener('dblclick', event => {
+    event.target.style.transform = 'rotate(90deg)'
+    event.target.style.transition = '3s'
+})
+
+let pic = document.querySelector("img")
+
+// function zoom(event) {
+//     event.preventDefault();
+//     scale += event.deltaY * -0.01;
+//     scale = Math.min(Math.max(.125, scale), 4);
+//     pic.target.style.transform = `scale(${scale})`;
+//   }
+
+  pic.addEventListener('wheel', event => {
+      event.target.style.transform = "scale(.5)"
+      event.target.style.transition ="3s ease-in-out"
+  })
+
+  let footer = document.querySelector("footer")
+
+  footer.addEventListener("mouseenter", event => {
+      event.target.style.fontSize ="22px"
+      event.target.style.backgroundColor = "blue"
+  })
+
+//   window.addEventListener("load", checkCookies())
+  
+
+// function checkCookies() {
+//   var text = "";
+
+//   if (navigator.cookieEnabled == true) {
+//     text = "Cookies are enabled.";
+//   } else {
+//      text = "Cookies are not enabled.";
+//   }
+// }
+
+let lastSection = document.querySelectorAll("section")
+
+lastSection[1].addEventListener("click", event => {
+    event.target.style.background = 'url(https://images.unsplash.com/photo-1568145675395-66a2eda0c6d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80)'
+})
